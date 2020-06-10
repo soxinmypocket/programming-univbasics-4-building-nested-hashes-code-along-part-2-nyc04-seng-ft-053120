fb303a43bbe60d9e8336204e8b4211be24fa2db5
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-10 19:02:06 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -59,9 +59,9 @@ def add_characters
    #   - The first hash should have the :name key set to "Prince Escalus"
    #   - The second hash should have the :name key set to "Apothecary"
    # Code your solution here:
-epic_tragedy[:Montague][:hero][:status] = "dead"
+epic_tragedy[:Montague][:hero][:status] == "dead"
 
-epic_tragedy[:Capulet][:heroine][:status] = "dead"
+epic_tragedy[:Capulet][:heroine][:status] == "dead"
 
 
 
